968d53606a2216d4a6bc47c7fbb6b669a0ac699b
5CG8101XK6\Anzen <juan.cruz@agilethought.com>
2021-12-23 15:09:50 -0600

    first commit

---
+++ src/main/java/mx/com/santander/consulta/ConsultaServiceApplication.java
@@ -0,0 +1,13 @@
+package mx.com.santander.consulta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultaServiceApplication.class, args);
+	}
+
+}
