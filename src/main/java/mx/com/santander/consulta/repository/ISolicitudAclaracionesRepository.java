c69fddc4846d5a2e540746c88eb585c4beada941
5CG8101XK6\Anzen <juan.cruz@agilethought.com>
2022-01-18 18:56:52 -0600

    Se cambia el nombre del paquete

---
+++ src/main/java/mx/com/santander/consulta/repository/ISolicitudAclaracionesRepository.java
@@ -0,0 +1,31 @@
+package mx.com.santander.consulta.repository;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import mx.com.santander.consulta.consultas.ConsultaReporte;
+import mx.com.santander.consulta.model.ConsultaSolicitudAclaracionesResponse;
+import mx.com.santander.consulta.model.ConsultaSolicitudesAclaracionesRequest;
+
+/**
+ * @author Anzen
+ *
+ */
+public interface ISolicitudAclaracionesRepository {
+
+	/**
+	 * 
+	 * @param consultaSolicitudesAclaracionesRequest
+	 * @return
+	 */
+	ConsultaSolicitudAclaracionesResponse getConsulta(ConsultaSolicitudesAclaracionesRequest consultaSolicitudesAclaracionesRequest);
+
+	/**
+	 * @param jdbcTemplate
+	 */
+	void setRepository(JdbcTemplate jdbcTemplate);
+
+	/**
+	 * @param consultas
+	 */
+	void setConsultas(ConsultaReporte consultas);
+}
